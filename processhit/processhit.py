c3388d817b7eb6bdb967314c500656a446742478
Maurice Manning <maurice.manning@gmail.com>
2018-03-22 22:33:32 +0800

    Update processhit.py

--- processhit/processhit.py
+++ processhit/processhit.py
@@ -1,11 +1,24 @@
+import logging
+
 class ProcessHit(object):
 
-  
-
-	def on_get(self, req, resp):
-		payload = {}
-    resp.body = json.dumps(payload)
-		resp.status = falcon.HTTP_200
+    # set up logging to file
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(funcName)s():  %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M:%S',
+                    filename='/code/logs/gtt.log',
+                    filemode='w')
+	
+    def on_get(self, req, resp):
+        payload = {'foo':'bar'}
+	resp.body = json.dumps(payload)
+	resp.status = falcon.HTTP_200
     
-	def on_post(self, req, resp):
-		payload = {}
+
+    def on_post(self, req, resp):	
+        """Handles POST requests"""
+        try:
+            raw_json = req.stream.read()
+	    logging.info('on_post: ' + str(raw_json))
+        except Exception as ex:
+            raise falcon.HTTPError(falcon.HTTP_400,'Error',ex.message)
